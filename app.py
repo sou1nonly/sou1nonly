3f6765c3a5263eb7c69226971a2d8d76871ffc4c
Neeraj Thakur <97724487+sou1nonly@users.noreply.github.com>
2024-12-03 02:33:57 +0530

    app.py

--- app.py
+++ app.py
@@ -2,45 +2,7 @@ import streamlit as st
 import joblib
 import string
 import spacy
-import subprocess
-import sys
-import os
 
-def install(package):
-    """Install a package using pip."""
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# Get the current directory where the script is located
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Path to the wheel file
-wheel_file = os.path.join(current_directory, "en_core_web_sm-3.5.0-py3-none-any.whl")
-
-# Install the package if it is not already installed
-try:
-    import spacy
-except ImportError:
-    print("Spacy is not installed. Installing...")
-    install("spacy")
-
-# Check if the model is already installed
-try:
-    nlp = spacy.load("en_core_web_sm")
-    print("Model 'en_core_web_sm' is already installed.")
-except OSError:
-    print("Model 'en_core_web_sm' is not installed. Installing from wheel file...")
-    install(wheel_file)
-    nlp = spacy.load("en_core_web_sm")
-
-# Example text to process
-text = "Hello, world! This is a test sentence."
-
-# Process the text
-doc = nlp(text)
-
-# Print tokens and their part-of-speech tags
-for token in doc:
-    print(token.text, token.pos_)
 
 st.set_page_config(page_title="Spam Email Detector", page_icon="✉️")
 
